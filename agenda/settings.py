26e84779ce3d274726d96dd142def9a19a4600ff
IgorVidal <igorcostavidal@gmail.com>
2022-11-25 09:58:21 -0300

    pt-br

--- agenda/settings.py
+++ agenda/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+#Import for bootstrap messages alerts
+from django.contrib.messages import constants
 
 from pathlib import Path
 
@@ -104,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -124,3 +126,16 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = Path(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
+
+#messages with bootstrap
+
+MESSAGE_TAGS = {
+    constants.ERROR: 'alert-danger',
+    constants.WARNING: 'alert-warning',
+    constants.DEBUG: 'alert-info',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+}
--- contatos/views.py
+++ contatos/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 from django.http import Http404
 from django.db.models import Q, Value
 from django.db.models.functions import Concat
+from django.contrib import messages
 from .models import Contato
 
 # Create your views here.
@@ -25,9 +26,9 @@ def ver_contato(request, contato_id):
 
 def busca(request):
     termo = request.GET.get("termo")
-    if termo is None:
-        raise Http404()
-    
+    if termo is None or not termo:
+        messages.add_message(request, messages.ERROR, "Campo não pode ser vazio.")
+        return redirect("index")
     campo = Concat("nome", Value(" "), "sobrenome")
 
     contatos = Contato.objects.annotate(nome_completo=campo).filter(
